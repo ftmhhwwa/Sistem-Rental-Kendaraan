b547310b671890775694d2293374db33e0cd8c23
ftmhhwwa <fatimahhawwa0201@gmail.com>
2025-11-27 16:31:02 +0700

    menambahkan tabel juga data dummy kendaraan dan pelanggan

---
+++ rental-kendaraan/sql/dummy_kendaraan.sql
@@ -0,0 +1,42 @@
+INSERT INTO kendaraan (no_polisi, merk, model, tahun, harga_dasar, status, jenis) VALUES
+-- MOBIL (6)
+('B1001AA', 'Toyota', 'Avanza', 2020, 350000, 'Tersedia', 'Mobil'),
+('B1002BB', 'Honda', 'Brio', 2022, 300000, 'Tersedia', 'Mobil'),
+('B1003CC', 'Suzuki', 'Ertiga', 2021, 330000, 'Tersedia', 'Mobil'),
+('B1004DD', 'Daihatsu', 'Xenia', 2019, 320000, 'Tersedia', 'Mobil'),
+('B1005EE', 'Mitsubishi', 'Xpander', 2023, 400000, 'Tersedia', 'Mobil'),
+('B1006FF', 'Wuling', 'Almaz', 2022, 380000, 'Tersedia', 'Mobil'),
+
+-- MOTOR (6)
+('B2001AA', 'Honda', 'Vario 150', 2021, 120000, 'Tersedia', 'Motor'),
+('B2002BB', 'Yamaha', 'NMax', 2022, 150000, 'Tersedia', 'Motor'),
+('B2003CC', 'Suzuki', 'Satria F150', 2020, 100000, 'Tersedia', 'Motor'),
+('B2004DD', 'Kawasaki', 'Ninja 250', 2019, 250000, 'Tersedia', 'Motor'),
+('B2005EE', 'Honda', 'Beat', 2023, 110000, 'Tersedia', 'Motor'),
+('B2006FF', 'Yamaha', 'Aerox', 2021, 140000, 'Tersedia', 'Motor'),
+
+-- BUS (5)
+('B3001AA', 'Mercedes Benz', 'OH1526', 2018, 1500000, 'Tersedia', 'Bus'),
+('B3002BB', 'Hino', 'RK8', 2019, 1400000, 'Tersedia', 'Bus'),
+('B3003CC', 'Isuzu', 'ELF Bus', 2020, 900000, 'Tersedia', 'Bus'),
+('B3004DD', 'Hyundai', 'County', 2021, 1000000, 'Tersedia', 'Bus'),
+('B3005EE', 'Toyota', 'Coaster', 2022, 1100000, 'Tersedia', 'Bus'),
+
+-- TRUCK (5)
+('B4001AA', 'Hino', 'Dutro', 2019, 800000, 'Tersedia', 'Truck'),
+('B4002BB', 'Isuzu', 'Giga', 2018, 900000, 'Tersedia', 'Truck'),
+('B4003CC', 'Mitsubishi', 'Fuso', 2020, 950000, 'Tersedia', 'Truck'),
+('B4004DD', 'Volvo', 'FMX', 2021, 1500000, 'Tersedia', 'Truck'),
+('B4005EE', 'Scania', 'R500', 2022, 2000000, 'Tersedia', 'Truck'),
+
+-- MOTOR LISTRIK (4)
+('B5001AA', 'Gesits', 'G1', 2022, 90000, 'Tersedia', 'MotorListrik'),
+('B5002BB', 'Viar', 'Q1', 2021, 85000, 'Tersedia', 'MotorListrik'),
+('B5003CC', 'Yadea', 'G5', 2023, 100000, 'Tersedia', 'MotorListrik'),
+('B5004DD', 'Selis', 'E-Max', 2019, 75000, 'Tersedia', 'MotorListrik'),
+
+-- MOBIL LISTRIK (4)
+('B6001AA', 'Hyundai', 'Ioniq 5', 2023, 700000, 'Tersedia', 'MobilListrik'),
+('B6002BB', 'Tesla', 'Model 3', 2022, 1000000, 'Tersedia', 'MobilListrik'),
+('B6003CC', 'Wuling', 'Air EV', 2023, 400000, 'Tersedia', 'MobilListrik'),
+('B6004DD', 'Nissan', 'Leaf', 2021, 650000, 'Tersedia', 'MobilListrik');
---
+++ rental-kendaraan/sql/dummy_pelanggan.sql
@@ -0,0 +1,11 @@
+INSERT INTO pelanggan (nama, nik, no_hp, alamat) VALUES
+('Aldi Pratama', '3201010202030001', '081234567001', 'Bandung'),
+('Rina Kurniawati', '3201010202030002', '081234567002', 'Jakarta'),
+('Dedi Sulistyo', '3201010202030003', '081234567003', 'Bekasi'),
+('Siti Rahmawati', '3201010202030004', '081234567004', 'Depok'),
+('Budi Santoso', '3201010202030005', '081234567005', 'Bogor'),
+('Lina Marlina', '3201010202030006', '081234567006', 'Cimahi'),
+('Fajar Hidayat', '3201010202030007', '081234567007', 'Subang'),
+('Putri Ayu', '3201010202030008', '081234567008', 'Cirebon'),
+('Andi Wijaya', '3201010202030009', '081234567009', 'Karawang'),
+('Salsa Amelia', '3201010202030010', '081234567010', 'Sumedang');
